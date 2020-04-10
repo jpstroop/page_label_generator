41e252ba222a825de1f92e27c2a6b7036b2589e8
Jon Stroop <jstroop@badapple-mothership.localdomain>
2020-04-10 11:14:09 -0400

    update all dependencies

--- spec/run.js
+++ spec/run.js
@@ -1,5 +1,12 @@
 import Jasmine from 'jasmine'
 
 var jasmine = new Jasmine()
+
+jasmine.configureDefaultReporter({
+    timer: new jasmine.jasmine.Timer(),
+    showColors: true,
+});
+
 jasmine.loadConfigFile('spec/support/jasmine.json')
+
 jasmine.execute()
